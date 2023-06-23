6146acd942c04aba5d528a859dd3d8133e3641bc
Rémi Sarrailh <remi@usini.eu>
2023-06-23 02:39:23 +0200

    Fix rewriterLiens ne marche pas toujours

--- includer.js
+++ includer.js
@@ -1,22 +1,23 @@
 includeHTML();
 
 function rewriterLiens() {
-  var liens = document.querySelectorAll("a, img");
   var urlActuelle = window.location.origin;
-  
-  liens.forEach(function(lien) {
-    var url = lien.getAttribute("href") || lien.getAttribute("src");
-    
-    if (url && url.includes("github.io")) {
-      var nouvelleUrl = url.replace("/", "/labsud_web");
-      
-      if (lien.tagName === "a") {
-        lien.setAttribute("href", nouvelleUrl);
-      } else if (lien.tagName === "img") {
-        lien.setAttribute("src", nouvelleUrl);
+  if (urlActuelle.includes("github.io")) {
+    var liens = document.querySelectorAll("a, img");
+    liens.forEach(function (lien) {
+      var url = lien.getAttribute("href") || lien.getAttribute("src");
+
+      if (url) {
+        var nouvelleUrl = url.replace("/", "/labsud_web");
+        console.log(nouvelleUrl);
+        if (lien.tagName === "a") {
+          lien.setAttribute("href", nouvelleUrl);
+        } else if (lien.tagName === "img") {
+          lien.setAttribute("src", nouvelleUrl);
+        }
       }
-    }
-  });
+    });
+  }
 }
 
 function includeHTML() {
@@ -30,10 +31,10 @@ function includeHTML() {
     if (file) {
       /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
+      xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+          if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
           /* Remove the attribute, and call this function once more: */
           elmnt.removeAttribute("w3-include-html");
           includeHTML();
@@ -41,15 +42,15 @@ function includeHTML() {
       }
       xhttp.open("GET", file, true);
       xhttp.send();
-     
-     
+
+
       /* Exit the function: */
       return;
     }
 
   }
   // Show page after load
-  document.querySelector('.wait_for_load').removeAttribute("class","wait_for_load")
+  document.querySelector('.wait_for_load').removeAttribute("class", "wait_for_load")
   rewriterLiens();
   checkOpeningHours();
 }
